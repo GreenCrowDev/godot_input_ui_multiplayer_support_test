93706d7f98b35e6502b4f505ebc32ffd707e3f25
matricola787 <pcalifano@outlook.it>
2025-02-16 13:14:26 +0100

    Implement arrow keys for up to 4 players.

--- input_test.gd
+++ input_test.gd
@@ -2,6 +2,27 @@ extends Node3D
 
 @export var start_focus_buttons: Array[Button]
 
+var wasd_dict: Dictionary = {
+	"ui_up": KEY_W,
+	"ui_left": KEY_A,
+	"ui_down": KEY_S,
+	"ui_right": KEY_D,
+}
+
+var tfgh_dict: Dictionary = {
+	"ui_up": KEY_T,
+	"ui_left": KEY_F,
+	"ui_down": KEY_G,
+	"ui_right": KEY_H,
+}
+
+var ijkl_dict: Dictionary = {
+	"ui_up": KEY_I,
+	"ui_left": KEY_J,
+	"ui_down": KEY_K,
+	"ui_right": KEY_L,
+}
+
 func _ready() -> void:
 	for i in start_focus_buttons.size():
 		start_focus_buttons[i].call_thread_safe("grab_focus", i)
@@ -13,30 +34,26 @@ func _process(delta: float) -> void:
 func _unhandled_key_input(event: InputEvent):
 	if event is InputEventKey:
 		# print("Device: %s; Player: %s;" % [event.device, event.get_player()])
-		manage_ijkl(event)
+		manage_multiplayer_arrow_keys(event, wasd_dict, Input.PLAYER_2)
+		manage_multiplayer_arrow_keys(event, tfgh_dict, Input.PLAYER_3)
+		manage_multiplayer_arrow_keys(event, ijkl_dict, Input.PLAYER_4)
 	else:
 		print(event)
 
 func _input(event: InputEvent):
-	if event.player == Input.PLAYER_2:
-		print("Player 2 Input.")
-		pass
-	elif event is InputEventAction:
-		print(event)
-		pass
+	if event is InputEventMouseMotion:
+		return
+	
+	# TODO: event.player is the mask, not the number id you would expect.
+	# e.g. Input.PLAYER_3 == 4
+	print("Player %s Input. %s" % [event.player, event])
 
-func manage_ijkl(event: InputEventKey) -> void:
-	var ijkl_dict: Dictionary = {
-		"ui_up": KEY_I,
-		"ui_left": KEY_J,
-		"ui_down": KEY_K,
-		"ui_right": KEY_L,
-	}
-	for key in ijkl_dict:
-		if event.player != Input.PLAYER_2 && event.keycode == ijkl_dict[key] && event.is_pressed():
+func manage_multiplayer_arrow_keys(event: InputEventKey, keys_dict: Dictionary, player: int) -> void:
+	for key in keys_dict:
+		if event.player != Input.PLAYER_2 && event.keycode == keys_dict[key] && event.is_pressed():
 			get_viewport().set_input_as_handled()
-			var p2_event = InputEventAction.new()
-			p2_event.player = Input.PLAYER_2
-			p2_event.action = key
-			p2_event.pressed = true
-			Input.parse_input_event(p2_event)
+			var key_event = InputEventAction.new()
+			key_event.player = player
+			key_event.action = key
+			key_event.pressed = true
+			Input.parse_input_event(key_event)
