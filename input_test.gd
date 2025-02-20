2a3608d5af1bf64b19f0d3caef847ec004a6ec4d
GreenCrowDev <pcalifano@outlook.it>
2025-02-20 21:59:53 +0100

    Add joypad mapping

--- input_test.gd
+++ input_test.gd
@@ -1,6 +1,11 @@
 extends Node3D
 
-@export var start_focus_buttons: Array[Button]
+@onready var start_focus_buttons : Array[Control] = [
+	$CanvasLayer/MarginContainer/GridContainer/PlayerControl1/GridContainer/Button,
+	$CanvasLayer/MarginContainer/GridContainer/PlayerControl2/GridContainer/Button,
+	$CanvasLayer/MarginContainer/GridContainer/PlayerControl3/GridContainer/Button,
+	$CanvasLayer/MarginContainer/GridContainer/PlayerControl4/GridContainer/Button
+]
 
 var wasd_dict: Dictionary = {
 	"ui_up": KEY_W,
@@ -22,15 +27,19 @@ var ijkl_dict: Dictionary = {
 	"ui_down": KEY_K,
 	"ui_right": KEY_L,
 }
-
+	
 func _ready() -> void:
+	# start_focus_buttons[0].grab_focus(PLAYER_ID_P1)
 	print("Max Players: %s" % Input.PLAYERS_MAX)
 	for i in start_focus_buttons.size():
 		start_focus_buttons[i].call_thread_safe("grab_focus", i)
 		pass
+	
+	# Map joypad 0 to player 4.
+	Input.set_joy_player_id(0, PLAYER_ID_P4)
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_select", false, Input.PLAYER_ALL):
+	if Input.is_action_just_pressed("ui_select", false, PLAYER_ID_P1):
 		for i in start_focus_buttons.size():
 			print(start_focus_buttons[i].get_focused_players_id())
 
